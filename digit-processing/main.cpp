8a7bdcf03fce4b524b84cf9f6d3df87069fd11f2
Mike Sharp <msharp23@hotmail.co.uk>
2016-09-04 19:59:32 +0100

    some more stuff about processing single characters into a number

---
+++ digit-processing/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void charsToNumber();
+void commaSeparatedNumbers();
+
+int main()
+{
+    commaSeparatedNumbers();
+    return 0;
+}
+
+
+void charsToNumber(){
+    cout << "Enter a number with as many digits as you like" << endl;
+    char digitChar = cin.get();
+    int number = (digitChar - '0');
+    digitChar = cin.get();
+
+    while(digitChar != 10){
+        number = number * 10 + (digitChar - '0');
+        digitChar = cin.get();
+    }
+
+    cout << "Number entered: " << number << endl;
+}
+
+void commaSeparatedNumbers(){
+    char digitChar;
+
+    do{
+            digitChar = cin.get();
+            int number = (digitChar - '0');
+            digitChar = cin.get();
+
+            while(digitChar != 10 && digitChar != ','){
+                number = number * 10 + (digitChar - '0');                digitChar = cin.get();
+            }
+
+            cout << "Number entered: " << number << endl;
+    }while(digitChar != 10);
+
+
+}
